55e8907d24fdea076df0f5ad5bc7b7e6f34573c4
meninol2023-cell <meninol2023@gmail.com>
2026-04-16 21:34:40 -0300

    Update index.js

--- bot-musica/index.js
+++ bot-musica/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Bot online!");
 });
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor web ligado");
 });
 
